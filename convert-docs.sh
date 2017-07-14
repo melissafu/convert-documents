ca112f73fde151049e5f17c98b2906755f6c428d
melissafu727 <melissafu727@gmail.com>
2017-07-14 12:31:45 -0400

    basic script

--- convert-docs.sh
+++ convert-docs.sh
@@ -1,13 +1,20 @@
 
 
 # Declare input argument as a variable
+INPUTFILE=$1
+OUTPUTNAME=$INPUTFILE
 
 # Convert markdown to HTML
+pandoc -o $OUTPUTNAME.html $INPUTFILE  
 
 # Convert markdown to DOCX
+pandoc -o $OUTPUTNAME.docx $INPUTFILE  
 
 # Convert markdown to ODT
+pandoc -o $OUTPUTNAME.odt $INPUTFILE  
 
 # Convert markdown to PDF
+pandoc -o $OUTPUTNAME.pdf $INPUTFILE  
 
 # Make the command line read "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF
+echo "Converted INPUT-FILENAME to HTML, DOCX, ODT, PDF"
